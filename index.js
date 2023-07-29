aea857f3f37b478b5cca67ec7aab9ba3d3832827
JaimeBaldin <73400106+JaimeBaldin@users.noreply.github.com>
2023-07-29 15:29:02 -0300

    configurado view engine, static files e body parser

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 
 const app = express();
 
+//View engine
+app.set("view engine", "ejs");
 
+//Static files
+app.use(express.static("public"));
+
+//Body Parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get("/", (req,res)=> {
     res.send("Bem vindo")
