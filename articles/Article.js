68ffe1f6405187d95b07683d9c6384dbba3ee758
JaimeBaldin <73400106+JaimeBaldin@users.noreply.github.com>
2023-07-30 06:22:35 -0300

    Criado models de categoria e artigos

---
+++ articles/Article.js
@@ -0,0 +1,17 @@
+import { Sequelize } from "sequelize";
+import connection from "./database/database.js";
+
+const Article = connection.define('articles', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },slug: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },body: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    }    
+})
+
+export default Article;
