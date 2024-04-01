9e1ceb64f841a4547c98791a27c123f896bc6517
JaimeBaldin <73400106+JaimeBaldin@users.noreply.github.com>
2024-04-01 05:53:50 -0300

    Salvando cadastro de categorias no banco. Instalado biblioteca slugify

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,9 +1,25 @@
 import express from 'express';
 
 const router = express.Router();
+import Category from "./Category.js";
+import slugify from "slugify";
 
 router.get('/admin/categories/new', (req, res) => {
     res.render("admin/categories/new");
+});
+
+router.post("/catgories/save", (req, res) => {
+    var title = req.body.title;
+    if (title != undefined) {
+        Category.create({
+            title: title,
+            slug: slugify(title),
+        }).then(() => {
+            res.redirect("/");
+        });
+    } else {
+        res.redirect("/admin/categories/new");
+    }
 });
 
 export default router;
